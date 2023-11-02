cb92c4eafe39eef3d2e5519a286a3d6a29eaba4a
jacobnormington <jacobnormington@gmail.com>
2023-11-02 19:38:11 +0000

    Moved spin_lock into its own file, for reference elsewhere

---
+++ lib/synchronization.hpp
@@ -0,0 +1,24 @@
+#ifndef SYNC_H
+    #define SYNC_H
+
+    #include <atomic>
+
+    /* A busy waiting lock for accessing the history table and some other resources. 
+        - This is ideal over a mutex lock when access is expected to be a very short operation. 
+        - There may be room for OS / hardware specific preformance enchancements, but it seems unlikely to provide much benefit.
+        - Does not prevent starvation but I doubt it will impact preformance. */
+    class spin_lock{
+        public:
+            /* Wait until the lock is released, then reserve it for yourself. */
+            void lock() {
+                while (lck.test_and_set(std::memory_order_acquire)) { ; }
+            };
+            /* Release the lock for others to use. */
+            void unlock() {
+                lck.clear(std::memory_order_release);
+            };
+        private:
+            std::atomic_flag lck = ATOMIC_FLAG_INIT;
+    };
+
+#endif
